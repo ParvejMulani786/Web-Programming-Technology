2a2ef66f5224dace3a8da61a04c285771c815157
PARVEJ <ceparvejmulani@gmail.com>
2023-01-06 22:45:22 +0530

    Web Programming Technology:

---
+++ JavaScript/first.js
@@ -0,0 +1,46 @@
+// var x=45;
+// console.log(x,typeof(x))
+
+// x=2.5
+// console.log(x,typeof(x))
+
+// x='p'
+// console.log(x,typeof(x))
+
+// x='Parvej'
+// console.log(x,typeof(x))
+
+// x="Parvej"
+// console.log(x,typeof(x))
+
+// x=true
+// console.log(x,typeof(x))
+
+
+// x=`this if first line
+// this is second line`
+// console.log(x,typeof(x))
+
+// var y=30
+// x="Rohans is going to turn his" +y+"yes "
+// x=`the age of Ram is I guess
+//  above ${y}`
+//  //x='Rohans is going to turn his ${y} yes '
+// console.log(x)
+
+var a=15
+console.log(a)
+var a=48
+console.log(a)
+
+let b=12
+console.log(b)
+//let b=45   //gives error
+
+//hoisting
+
+// console.log(b)  //cannot acces before initilization
+// let b=12
+const c=2.5
+c=45;
+console.log(c) //cannot edit constant
